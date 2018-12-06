1298123fdc5cd90bcf392a35ac6b2111bd5ad484
Katelyn Varkonyi <kvarkonyi@gmail.com>
2018-12-05 19:30:44 -0600

    Fixed issues with scroll view. Fixed table view to have to toolbar for navigation.

---
+++ BoomAppMobile/Controllers/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  BoomAppMobile
+//
+//  Created by Katelyn on 12/4/18.
+//  Copyright © 2018 Katelyn. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var toolbar: UIToolbar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
